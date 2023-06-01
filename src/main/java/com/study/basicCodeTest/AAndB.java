5d5dbdf38f1fcd096a4cd33898c8773f04763db0
jaekangchoi <jkchoi7091@knou.ac.kr>
2023-06-01 15:50:48 +0900

    문자열 반복은 repeat메서드 활용. close로 닫아주자

---
+++ src/main/java/com/study/basicCodeTest/AAndB.java
@@ -0,0 +1,13 @@
+package com.study.basicCodeTest;
+import java.util.Scanner;
+public class AAndB {
+    //정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+ a + "\nb = "+ b);
+    }
+}
