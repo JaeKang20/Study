54f76784f5b676871137f696f39da289f0d2bc74
jaekangchoi <jkchoi7091@knou.ac.kr>
2023-06-09 16:43:21 +0900

    코딩기초 트레이닝 | 프로그래머스 문제

---
+++ src/main/java/com/study/basicCodeTest/PrintSpecialChararcter.java
@@ -0,0 +1,10 @@
+package com.study.basicCodeTest;
+
+public class PrintSpecialChararcter {
+    //!@#$%^&*(\'"<>?:;
+    //다음과 같이 출력되도록 하라
+
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
