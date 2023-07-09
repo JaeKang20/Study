3d8c3c931819d24a472b8d5ba49602172bd3f7ae
jaekangchoi <jkchoi7091@knou.ac.kr>
2023-07-09 22:30:32 +0900

    정처기 정리

--- src/main/java/com/study/heckerRank/WelcomeToJava.java
+++ src/main/java/com/study/heckerRank/WelcomeToJava.java
@@ -1,5 +1,10 @@
 package com.study.heckerRank;
 
 public class WelcomeToJava {
-
+    public static void main(String[] args) {
+        int x = 1;
+        int y =6;
+        
+        System.out.println("x="+ x + "y="+y);
+    }
 }
